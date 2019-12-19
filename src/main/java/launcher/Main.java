082ef1ec8f2f5ab20c2f0be691a5c5577eacff48
IN1DM3B_13 <IN1DM3B_13@H015112.NET>
2019-12-19 10:01:17 +0100

    se meten los diferentes XMLs en alojamientos

--- src/main/java/launcher/Main.java
+++ src/main/java/launcher/Main.java
@@ -1,9 +1,13 @@
 package launcher;
+import java.io.File;
+import java.util.ArrayList;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import objetos.Administrador;
+import objetos.Alojamiento;
 
 public class Main {
 
@@ -15,6 +19,18 @@ public class Main {
 		
 		Transaction tx= session.beginTransaction();
 
+		DescargaWeb descarga = new DescargaWeb();
+		File archivo = new File("alojTuristicos.xml");
+		File archivo1 = new File("alojRural.xml");
+		File archivo2 = new File("campings.xml");
+		ArrayList<Alojamiento> aloj = new ArrayList<Alojamiento>();
+		descarga.hacerDescarga(archivo);
+		descarga.hacerDescarga(archivo1);
+		descarga.hacerDescarga(archivo2);
+		descarga.leerTag(archivo, aloj);
+		descarga.leerTag(archivo1, aloj);
+		descarga.leerTag(archivo2, aloj);
+		System.out.println(aloj.size());
 	}
 
 }
